1633756df119910e00ddb7960737883ca9dcad5a
Lawliet-Chan <1576710154@qq.com>
2025-08-19 11:27:43 +0800

    update blockchain client

--- backend/blockchain/client.go
+++ backend/blockchain/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"math/big"
-	"scos/config"
 	"strings"
 
 	"github.com/ethereum/go-ethereum"
@@ -21,11 +20,11 @@ type BlockchainClient struct {
 	auth       *bind.TransactOpts
 }
 
-func NewBlockchainClient(chainInfos map[string]config.ChainInfo, privateKeyHex string) (*BlockchainClient, error) {
+func NewBlockchainClient(rpcUrls map[string]string, privateKeyHex string) (*BlockchainClient, error) {
 	clients := make(map[string]*ethclient.Client)
 
-	for chain, info := range chainInfos {
-		client, err := ethclient.Dial(info.RPC)
+	for chain, url := range rpcUrls {
+		client, err := ethclient.Dial(url)
 		if err != nil {
 			return nil, err
 		}
@@ -95,6 +94,55 @@ func (bc *BlockchainClient) StakeStock(chain, contractAddr, tokenAddr string, am
 
 	boundContract := bind.NewBoundContract(common.HexToAddress(contractAddr), parsedABI, client, client, client)
 	tx, err := boundContract.Transact(bc.auth, "stakeStock", common.HexToAddress(tokenAddr), amount, scosAmount)
+	if err != nil {
+		return "", err
+	}
+
+	return tx.Hash().Hex(), nil
+}
+
+func (bc *BlockchainClient) UnstakeStock(chain, contractAddr, tokenAddr string) (string, error) {
+	client := bc.GetClient(chain)
+
+	contractABI := `[{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"unstakeStock","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
+
+	parsedABI, err := abi.JSON(strings.NewReader(contractABI))
+	if err != nil {
+		return "", err
+	}
+
+	data, err := parsedABI.Pack("unstakeStock", common.HexToAddress(tokenAddr))
+	if err != nil {
+		return "", err
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), bc.auth.From)
+	if err != nil {
+		return "", err
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	msg := ethereum.CallMsg{
+		From: bc.auth.From,
+		To:   &common.Address{},
+		Data: data,
+	}
+	gasLimit, err := client.EstimateGas(context.Background(), msg)
+	if err != nil {
+		gasLimit = 300000 // fallback
+	}
+
+	bc.auth.Nonce = big.NewInt(int64(nonce))
+	bc.auth.Value = big.NewInt(0)
+	bc.auth.GasLimit = gasLimit
+	bc.auth.GasPrice = gasPrice
+
+	boundContract := bind.NewBoundContract(common.HexToAddress(contractAddr), parsedABI, client, client, client)
+	tx, err := boundContract.Transact(bc.auth, "unstakeStock", common.HexToAddress(tokenAddr))
 	if err != nil {
 		return "", err
 	}

8703a45668c54679170d7b6ab3044048cad9cb6a
Lawliet-Chan <1576710154@qq.com>
2025-08-19 11:47:19 +0800

    update chain client

--- backend/blockchain/client.go
+++ backend/blockchain/client.go
@@ -17,7 +17,6 @@ import (
 type BlockchainClient struct {
 	clients    map[string]*ethclient.Client
 	privateKey *ecdsa.PrivateKey
-	auth       *bind.TransactOpts
 }
 
 func NewBlockchainClient(rpcUrls map[string]string, privateKeyHex string) (*BlockchainClient, error) {
@@ -36,20 +35,23 @@ func NewBlockchainClient(rpcUrls map[string]string, privateKeyHex string) (*Bloc
 		return nil, err
 	}
 
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(1))
-	if err != nil {
-		return nil, err
-	}
-
 	return &BlockchainClient{
 		clients:    clients,
 		privateKey: privateKey,
-		auth:       auth,
 	}, nil
 }
 
 func (bc *BlockchainClient) GetClient(chain string) *ethclient.Client {
 	return bc.clients[chain]
+}
+
+func (bc *BlockchainClient) auth(chain string) (*bind.TransactOpts, error) {
+	chainID, err := bc.GetClient(chain).ChainID(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewKeyedTransactorWithChainID(bc.privateKey, chainID)
+
 }
 
 func (bc *BlockchainClient) StakeStock(chain, contractAddr, tokenAddr string, amount *big.Int, scosAmount *big.Int) (string, error) {
@@ -67,7 +69,12 @@ func (bc *BlockchainClient) StakeStock(chain, contractAddr, tokenAddr string, am
 		return "", err
 	}
 
-	nonce, err := client.PendingNonceAt(context.Background(), bc.auth.From)
+	auth, err := bc.auth(chain)
+	if err != nil {
+		return "", err
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), auth.From)
 	if err != nil {
 		return "", err
 	}
@@ -78,7 +85,7 @@ func (bc *BlockchainClient) StakeStock(chain, contractAddr, tokenAddr string, am
 	}
 
 	msg := ethereum.CallMsg{
-		From: bc.auth.From,
+		From: auth.From,
 		To:   &common.Address{},
 		Data: data,
 	}
@@ -87,13 +94,13 @@ func (bc *BlockchainClient) StakeStock(chain, contractAddr, tokenAddr string, am
 		gasLimit = 300000 // fallback
 	}
 
-	bc.auth.Nonce = big.NewInt(int64(nonce))
-	bc.auth.Value = big.NewInt(0)
-	bc.auth.GasLimit = gasLimit
-	bc.auth.GasPrice = gasPrice
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = gasLimit
+	auth.GasPrice = gasPrice
 
 	boundContract := bind.NewBoundContract(common.HexToAddress(contractAddr), parsedABI, client, client, client)
-	tx, err := boundContract.Transact(bc.auth, "stakeStock", common.HexToAddress(tokenAddr), amount, scosAmount)
+	tx, err := boundContract.Transact(auth, "stakeStock", common.HexToAddress(tokenAddr), amount, scosAmount)
 	if err != nil {
 		return "", err
 	}
@@ -116,7 +123,12 @@ func (bc *BlockchainClient) UnstakeStock(chain, contractAddr, tokenAddr string) 
 		return "", err
 	}
 
-	nonce, err := client.PendingNonceAt(context.Background(), bc.auth.From)
+	auth, err := bc.auth(chain)
+	if err != nil {
+		return "", err
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), auth.From)
 	if err != nil {
 		return "", err
 	}
@@ -127,7 +139,7 @@ func (bc *BlockchainClient) UnstakeStock(chain, contractAddr, tokenAddr string) 
 	}
 
 	msg := ethereum.CallMsg{
-		From: bc.auth.From,
+		From: auth.From,
 		To:   &common.Address{},
 		Data: data,
 	}
@@ -136,13 +148,13 @@ func (bc *BlockchainClient) UnstakeStock(chain, contractAddr, tokenAddr string) 
 		gasLimit = 300000 // fallback
 	}
 
-	bc.auth.Nonce = big.NewInt(int64(nonce))
-	bc.auth.Value = big.NewInt(0)
-	bc.auth.GasLimit = gasLimit
-	bc.auth.GasPrice = gasPrice
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = gasLimit
+	auth.GasPrice = gasPrice
 
 	boundContract := bind.NewBoundContract(common.HexToAddress(contractAddr), parsedABI, client, client, client)
-	tx, err := boundContract.Transact(bc.auth, "unstakeStock", common.HexToAddress(tokenAddr))
+	tx, err := boundContract.Transact(auth, "unstakeStock", common.HexToAddress(tokenAddr))
 	if err != nil {
 		return "", err
 	}
